0ea9150f715f0ad4800a3f0be17110ca45b5e45d
Mounssif Koumila <moncef.koumila@gmail.com>
2024-03-20 23:19:02 +0000

    remove: cleaning code

--- index.js
+++ index.js
@@ -1,41 +1,6 @@
 const fs = require("fs");
 const http = require("http");
 
-/* //////////// */
-/* Blocking, Synchronous way */
-/* //////////// */
-/* 
-// to read a file from the filesystem
-const textInput = fs.readFileSync("./txt/input.txt", { encoding: "utf8" });
-
-const textOut = `This is what we know about the avocado: ${textInput}.\nCreated on ${new Date().toLocaleDateString("en-GB")}`;
-
-// to generate a file
-fs.writeFileSync("./txt/output.txt", textOut, { encoding: "utf8" });
-console.log("File was successfully written"); */
-
-/* //////////// */
-/* Non-blocking, Asynchronous way */
-/* //////////// */
-
-/* fs.readFile("./txt/start.txt", (err, data1) => {
-  if (err) return console.log("Error 💥");
-
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
-      fs.writeFile(
-        `./txt/output-async.txt`,
-        `${data2} \n${data3}`,
-        "utf-8",
-        (err) => {
-          console.log("Your file has been successfully written 🎉");
-        }
-      );
-    });
-  });
-});
- */
-
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 
 // Create an HTTP server
